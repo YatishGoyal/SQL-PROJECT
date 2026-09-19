1524b6aa4f9b79b0335e9d257d3f0ae7f4df4ea4
Yatish Goyal <yatishg008@gmail.com>
2026-09-19 15:08:30 +0530

    .

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
+import streamlit as st
 import psycopg2 as psql
 
+
 def connection():
-    conn = psql.connect(host="localhost",database = "Bill",user = "postgres",password = "010826",port = "5432")
+    conn = psql.connect(
+        host=st.secrets["database"]["host"],
+        database=st.secrets["database"]["database"],
+        user=st.secrets["database"]["user"],
+        password=st.secrets["database"]["password"],
+        port=st.secrets["database"]["port"]
+    )
+
     return conn
-    
-connection()
-conn = connection()
